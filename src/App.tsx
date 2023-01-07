e7b832d3ffabb36703afc7a9d71e72f223609d96
Pedro Veras <veraspedro1608@gmail.com>
2023-01-07 19:14:35 -0300

    estrutura da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
 
-function App() {
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
+export function App() {
   return (
-    <div className="App">
-      <h1>Teste</h1>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
-
-export default App
