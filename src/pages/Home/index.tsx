a8ef109b4a75884db99448ef441a6ec3adbc8a6a
Pedro Veras <veraspedro1608@gmail.com>
2023-01-09 18:09:41 -0300

    paginas e rotas: pagina history

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -20,13 +20,25 @@ export function Home(){
                         id="task"
                         type="text"
                         placeholder='Dê um nome para o seu projeto'
+                        list="task-suggestions"
                     />
+
+                    <datalist id='task-suggestions'>
+                        <option value="Projeto 1"></option>
+                        <option value="Projeto 2"></option>
+                        <option value="Projeto 3"></option>
+                        <option value="Projeto 4"></option>
+                        <option value="Banana"></option>
+                    </datalist>
 
                     <label htmlFor="minutesAmount">durante</label>
                     <MinutesAmountInput
                         id="minutesAmount"
                         type="number"
                         placeholder='00'
+                        step={5}
+                        min={5}
+                        max={60}
                     />
 
                     <span>minutos.</span>
