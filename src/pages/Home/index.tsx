bafc3bcc885360661058deffff09d656160e0c50
Pedro Veras <veraspedro1608@gmail.com>
2023-01-10 18:41:24 -0300

    formularios: resetando formulario

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -25,7 +25,7 @@ const newCycleFormValidationSchema = zod.object({
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home(){
-    const { register, handleSubmit, watch, formState } = useForm<NewCycleFormData>({
+    const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
         resolver: zodResolver(newCycleFormValidationSchema),
         defaultValues: {
             task: '',
@@ -35,9 +35,8 @@ export function Home(){
 
     function handleCreateNewCycle(data: NewCycleFormData) {
         console.log(data)
+        reset()
     }
-
-    console.log(formState.errors)
 
     const task = watch('task')
     const isSubmitDisabled = !task
