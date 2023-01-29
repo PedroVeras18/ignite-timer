b7233fcb1e8ffea5a440d194de0929955416a448
Pedro Veras <veraspedro1608@gmail.com>
2023-01-29 17:34:32 -0300

    contexto no react - convertendo para contexto / contexto no formulario

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -1,16 +1,17 @@
-import { useState, useEffect } from "react";
+import { useEffect, useContext } from "react";
 import { CountdownContainer, Separator } from "./styles";
 
 import { differenceInSeconds } from "date-fns";
+import { CyclesContext } from "../..";
 
-interface CountdownProps {
-    activeCycle: any;
-    setCycles: any;
-    activeCycleId: any;
-}
-
-export function Countdown({ activeCycle, setCycles, activeCycleId }){
-    const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+export function Countdown(){
+    const {
+        activeCycle,
+        activeCycleId,
+        markCurrentCycleAsFinished,
+        amountSecondsPassed,
+        setSecondsPassed
+    } = useContext(CyclesContext)
 
     const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
 
@@ -22,19 +23,11 @@ export function Countdown({ activeCycle, setCycles, activeCycleId }){
                 const secondsDifference = differenceInSeconds(new Date(), activeCycle.startDate)
 
                 if (secondsDifference >= totalSeconds) {
-                    setCycles((state) => state.map((cycle) => {
-                        if (cycle.id === activeCycleId) {
-                          return { ...cycle, finishedDate: new Date() }
-                        } else {
-                          return cycle
-                        }
-                      }),)
-
-                      setAmountSecondsPassed(totalSeconds)
-
-                      clearInterval(interval)
+                    markCurrentCycleAsFinished()
+                    setSecondsPassed(totalSeconds)
+                    clearInterval(interval)
                 } else {
-                    setAmountSecondsPassed(secondsDifference)
+                    setSecondsPassed(secondsDifference)
                 }
             }, 1000)
         }
@@ -42,7 +35,21 @@ export function Countdown({ activeCycle, setCycles, activeCycleId }){
         return () => {
             clearInterval(interval)
         }
-    }, [activeCycle, totalSeconds, activeCycleId])
+    }, [activeCycle, totalSeconds, activeCycleId, markCurrentCycleAsFinished])
+
+    const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
+
+    const minutesAmount = Math.floor(currentSeconds / 60)
+    const secondsAmount = currentSeconds % 60
+
+    const minutes = String(minutesAmount).padStart(2, '0')
+    const seconds = String(secondsAmount).padStart(2, '0')
+
+    useEffect(() => {
+        if (activeCycle) {
+            document.title = `${minutes}:${seconds}`
+        }
+    }, [minutes, seconds, activeCycle])
 
     return (
         <CountdownContainer>
